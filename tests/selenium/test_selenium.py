3eaa31458524c8dbca60483f4285a8aded3f59fc
Shaj-Amex <shajeamex@gmail.com>
2021-06-11 14:28:37 +0100

    Final Changes Module_3

--- tests/selenium/test_selenium.py
+++ tests/selenium/test_selenium.py
@@ -1,6 +1,8 @@
 import os
 import pytest
 import dotenv
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
 from todo_app import app
 from todo_app.trello_config import  TrelloConfig
 from todo_app.trello_client import create_trello_board,delete_trello_board, get_doing_lists_on_board, get_todo_lists_on_board
@@ -50,7 +52,20 @@ def driver():
     with webdriver.Chrome(executable_path=r'/Users/shajeethsushama/DevOps-Course-Starter/chromedriver') as driver:
         yield driver
 
-def test_task_journey(driver, app_with_temp_board):   
+def test_task_journey(driver: WebDriver, app_with_temp_board):   
     driver.get('http://localhost:5000/') 
-    time.sleep(30) 
-    assert driver.title == 'To-Do App'
+    time.sleep(5)
+    text_box: WebElement = driver.find_element_by_name('title')
+    text_box.send_keys("Test Todo")
+    time.sleep(5)
+    submit_button: WebElement = driver.find_element_by_name('submit')
+    submit_button.click()
+    time.sleep(5)
+    doing_button: WebElement = driver.find_element_by_name('doing-button')
+    doing_button.click()
+    time.sleep(5)
+    done_button: WebElement = driver.find_element_by_name('done-button')
+    done_button.click()
+    time.sleep(5)
+    item_text: WebElement = driver.find_element_by_name('item-text')
+    assert item_text.text == 'Test Todo - Done'
